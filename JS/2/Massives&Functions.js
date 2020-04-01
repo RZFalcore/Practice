7b96dd5cf5f6acea09076bb046317bfed84aca38
RZFalcore <box.rz.box@gmail.com>
2020-04-01 12:53:07 +0300

    2 done

--- JS/2/Massives&Functions.js
+++ JS/2/Massives&Functions.js
@@ -55,11 +55,66 @@ console.log(formatString("Curabitur ligula sapien, tincidunt non."));
 console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
 // вернется форматированная строка
 
-//5
+// 5
 
-const checkForSpam = function(string) {};
+const checkForSpam = function(string) {
+  const result =
+    string.toLowerCase().includes("spam") ||
+    string.toLowerCase().includes("sale");
+  return result;
+};
 
 console.log(checkForSpam("Latest technology news")); // false
-// console.log(checkForSpam("JavaScript weekly newsletter")); // false
-// console.log(checkForSpam("Get best sale offers now!")); // true
-// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+// 6
+
+// let input;
+// const numbers = [];
+// let total = 0;
+
+// do {
+//   input = prompt("Введите число");
+//   if (isNaN(input)) {
+//     alert("Не число!");
+//     continue;
+//   } else {
+//     numbers.push(Number(input));
+//   }
+// } while (input !== null);
+
+// for (const number of numbers) {
+//   total += number;
+// }
+
+// console.log(`Общая сумма чисел равна ${total}`);
+
+// 7
+console.log("7-----------------------");
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+const isLoginValid = function(login) {
+  return login.length >= 4 && login.length <= 16;
+};
+
+const isLoginUnique = function(logins, login) {
+  return !logins.includes(login);
+};
+
+const addLogin = function(logins, login) {
+  if (isLoginValid(login) === false) {
+    console.log("Ошибка! Логин должен быть от 4 до 16 символов");
+  } else if (isLoginUnique(logins, login) === false) {
+    console.log("Такой логин уже используется!");
+  } else {
+    logins.push(login);
+    console.log("Логин успешно добавлен!");
+  }
+};
+
+console.log(addLogin(logins, "Ajax")); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, "robotGoogles")); // 'Такой логин уже используется!'
+console.log(addLogin(logins, "Zod")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, "jqueryisextremelyfast")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
