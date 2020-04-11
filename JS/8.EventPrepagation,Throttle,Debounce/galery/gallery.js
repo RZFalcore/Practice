39dfc3df7c3d99be8e4d54d4b5a5701ad703bae7
RZFalcore <box.rz.box@gmail.com>
2020-04-11 21:45:37 +0300

    8 dops in progress

--- JS/8.EventPrepagation,Throttle,Debounce/galery/gallery.js
+++ JS/8.EventPrepagation,Throttle,Debounce/galery/gallery.js
@@ -4,6 +4,7 @@ import galleryItems from "./galleryItems.js";
 const gallery = document.querySelector(".js-gallery");
 const modalWindow = document.querySelector(".js-lightbox");
 const modalImg = document.querySelector(".lightbox__image");
+const modalContent = document.querySelector(".lightbox__content");
 const modalExitBtn = document.querySelector("[data-action=close-lightbox]");
 
 galleryItems.forEach((item) => {
@@ -27,7 +28,6 @@ galleryItems.forEach((item) => {
 });
 
 gallery.addEventListener("click", handleModalOpening);
-modalExitBtn.addEventListener("click", closeModal);
 
 function handleModalOpening(e) {
   e.preventDefault();
@@ -36,9 +36,30 @@ function handleModalOpening(e) {
   }
   modalImg.setAttribute("src", e.target.dataset.source);
   modalWindow.classList.add("is-open");
+
+  modalWindow.addEventListener("click", closeModal);
+  document.addEventListener("keydown", handleModal);
 }
 
 function closeModal(e) {
+  if (e.target !== modalExitBtn && e.target !== modalContent) {
+    return;
+  }
+
   modalImg.setAttribute("src", "");
   modalWindow.classList.remove("is-open");
+
+  modalWindow.removeEventListener("click", closeModal);
+  document.removeEventListener("keydown", handleModal);
+}
+
+function handleModal(e) {
+  console.log(e.target);
+  if (e.key === "Escape") {
+    closeModal();
+  }
+  console.log(e.target.nextSibling);
+  //   if (e.key === "ArrowRight") {
+  //     modalImg.setAttribute("src", e.target.nextSibling.src);
+  //   }
 }
